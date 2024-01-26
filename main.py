bd6f3a9db416f60bdb980e86b2a0911014be1af5
Sina Najafi <sinanajafiyellow44@gmail.com>
2024-01-26 01:44:59 -0800

    exam

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+fromflask_sqlalchemy imprt SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQL_ALCHEMY_DATABASE_URI'] = 'sqlite:///Book.sqlite3'
+db = SQLALchemy(app)
+
+
+class Book(db.Model):
+    id = db.column("id", db.integer, primarykey=True)
+    title = db.column(db.String())
+    author = db.column(db.String())
+    def __int__(self, title, author):
+        self.title = title
+        self.author = author
+
+
+
+@app.route('/book')
+def book():
+    render_template("index.html", Book=Book.query.all())
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
